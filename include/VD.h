adf7f3f94c19e456a91880a75c16ae313de7ffc3
jadelarosac <jadelarosac.tic@gmail.com>
2019-10-12 19:22:00 +0200

    clase vector dinamico

--- include/VD.h
+++ include/VD.h
@@ -1,6 +1,6 @@
-/* 
+/**
  * @file VD.h
- * @author carlosromero
+ * @author carlosromero y JoseAdelaRosa
  *
  * 
  */
@@ -8,7 +8,64 @@
 #ifndef VD_H
 #define VD_H
 
-
-
+template <class T>
+class VD
+{
+    private:
+        T* datos;//zona de memoria para almacenar los datos de tipo T
+        int n;//numero de datos almacenados
+        int reservados;//espacio asignado a datos
+        void resize (int nuevotam);
+        void Copiar (const VD<T> &v);
+        void Liberar ();
+    public:
+        /**
+         * @brief Contructor por defecto y con parametro
+         * @param tam: elementos a reservar para el vector dinamico
+         * @note si no se proporciona un valor para tam se tomara como 10
+         * 
+         */
+        VD (int tam=10);
+        /**19* @brief Constructor de copia
+         * @param original: vector dinamico origen
+         */
+        VD (const VD<T> &original);
+        /**
+         * @brief Destructor. Elimina la memoria asociada al vector dinamico
+         */
+        ~VD ();
+        /**
+         * @brief Operador de asignacion
+         * @param v: vector dinamico fuente
+         * @return una referencia al objeto al que apunta this
+         */
+        VD<T> &operator= (const VD<T> &v);
+        /**
+         * @brief Obtiene el numero de elementos almacenados en el vector dinamico
+         */
+        int size() const {return n;}
+        /**
+         * @brief Consulta y modifica el elemento i-esimo
+         * @param i: posicion del elemento
+         * @return una referencia al elemento i-esimo del vector dinamico
+         */
+        
+        T &operator[] (int i) {return datos[i];}//version no constante
+        const T &operator[] (int i) const {return datos[i];}//version constate
+        /**
+         * @brief Inserta un objeto en la posicion pos del vector dinamico
+         * @param d: objeto a insertar
+         * @param pos: posicion donde insertar.
+         * @pre pos debe estar comprendido entre 0 y size()
+         * @post aumenta en uno el vector dinamico
+         */
+        void Insertar (const T &d, int pos);
+        /**
+         * @brief Elimina el elemento en la posicion pos
+         *  @param pos: posicion del elemento a borrar.
+         */
+        void Borrar (int pos);
+};
+#include "VD.cpp"
 #endif /* VD_H */
 
--- src/VD.cpp
+++ src/VD.cpp
@@ -1,4 +1,92 @@
+//VD.cpp
+//No ponemos #include "VD.h", ya hemos incluido VD.cpp en el VD.h
+template <class T>
+void VD<T> :: resize(int nuevotam)
+{
+    T *aux = new T [nuevotam];
+    int minimo = (n<nuevotam)?n:nuevotam;
+    for (int i=0; i<minimo; i++)
+        aux[i] = datos[i];
+    
+    reservados = nuevotam;
+    n = minimo;
+    delete [] datos;
+    datos = aux;
+}
 
-#include "VD.h"
+template <class T>
+void VD<T> :: Copiar (const VD<T> &v)
+{
+    reservados = v.reservados;
+    n = v.n;
+    datos = new T [reservados];
+    for (int i=0; i<n; i++)
+        datos[i] = v.datos[i];
+}
 
-using namespace std;
+template <class T>
+void VD<T> :: Liberar()
+{
+    delete [] datos;
+}
+
+template <class T>//en otros sitios el class se sustituye por typename
+VD<T> :: VD (int tam)
+{
+    reservados = tam;
+    datos = new T [reservados];
+    n=0;
+}
+
+template <class T>
+VD<T> :: VD(const VD<T> &original)
+{
+    Copiar(original);
+}
+
+template <class T>
+VD<T> :: ~VD()
+{
+    Liberar();
+}
+
+template <class T>
+VD<T> & VD<T> :: operator=(const VD<T> & v)
+{
+    if (this != &v)
+    {
+        Liberar();
+        Copiar(v);
+    }
+    
+    return *this;
+}
+
+template <class T>
+void VD<T> :: Insertar (const T &d, int pos)
+{
+    /*segun estudios estadisticos, la mejor forma de usar un vector dinamico es
+     * que n < reservados/2, y cuando se supere esa cifra hacer un resize de
+     * 2*reservados*/
+    
+    if (n >= (reservados/2))
+        resize(2*reservados);
+    
+    for (int i=n; i > pos; i--)
+        datos[i] = datos[i-1];
+    
+    datos[pos] = d;
+    n++;
+}
+
+template <class T>
+void VD<T> :: Borrar (int pos)
+{
+    for (int i=pos; i<n-1; i++)
+        datos[i] = datos[i+1];
+    
+    n--;
+    
+    if (n < (reservados/4))
+        resize (reservados/2);
+}
